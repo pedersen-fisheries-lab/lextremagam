002c1d4bd25d15a9ab6bcb2e28fb04fc278bec6a
NatalieDupont <natalie.dupont2000@gmail.com>
2023-12-08 16:04:20 -0500

    starting analyses

---
+++ R/02-run_sim_gams.R
@@ -0,0 +1,13 @@
+#import data to run gams
+
+#set up parallel processing,
+
+#set PP loop to:
+#run gam,
+#run segment finder
+#extract segment_summary (or just peark-trough summary?)
+#extract key gam summary stats (edf, R^2?, AIC) - find a reason for these, if not, no
+#stop parallel processing
+#sort model variables in an organized way
+#save to file (rda?)
+
