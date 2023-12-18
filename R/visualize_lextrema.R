08397c2f793f316d3807143c67aad750d4014016
NatalieDupont <natalie.dupont2000@gmail.com>
2023-12-18 15:08:47 -0500

    staring on visualization code

---
+++ R/visualize_lextrema.R
@@ -0,0 +1,27 @@
+
+
+# plot_lextrema <- function(lextrema){
+#   stopifnot(any(class(lextrema)=="lextrema"),
+#             any(class(lextrema$model)=="lextrema"))
+#
+#   plot <- marginaleffects::plot_predictions(test_compr$model, by="x", conf_level=0.96)
+#
+#   #x and y -dependent
+#   plot <- plot+
+#     geom_point(aes(x = test_compr$model$model$x, y = test_compr$model$model$y))
+#
+#
+# }
+
+
+
+
+
+
+
+
+
+
+
+
+
