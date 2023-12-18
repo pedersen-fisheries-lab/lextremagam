8081f0b65357c81e09f7ac607aef9e4b1bfc3462
NatalieDupont <natalie.dupont2000@gmail.com>
2023-12-18 15:09:02 -0500

    adding documentation to new example data

--- R/data.R
+++ R/data.R
@@ -11,3 +11,18 @@
 #' }
 #' @source see data-raw
 "data_sig_sin"
+
+
+#' Comprehensive example curved data with all segment features present
+#'
+#' Practice and example dataset to text package functionality
+#'
+#' @format ## `data_compr`
+#' A data frame with 237 rows and 3 columns:
+#' \describe{
+#'   \item{x}{predictor value}
+#'   \item{y_true}{the true response value}
+#'   \item{y}{the response with noise}
+#' }
+#' @source see data-raw
+"data_compr"
