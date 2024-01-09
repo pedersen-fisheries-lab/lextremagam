78ef5072468bdf8564815427a13d0a73ecc2a33a
NatalieDupont <natalie.dupont2000@gmail.com>
2024-01-09 17:27:10 -0500

    Improving peakfinding function

--- R/quantify_lextrema.R
+++ R/quantify_lextrema.R
@@ -41,7 +41,7 @@ quantify_lextrema <- function(mod, var = NULL, step_size = NULL, conf_level= 0.9
       avg_inp_step_size <- range/unique_x
 
       step_size <- avg_inp_step_size/1000
-      warning(paste0("step_size not defined. step_size will be defined as the average step size/1000. Step_size = ", step_size))
+      warning(paste0("step_size not defined. step_size will be defined as the average x interval between datapoints/1000. Step_size = ", step_size))
     }
     if(is.numeric(step_size)){
       if(step_size <= 0){
@@ -130,22 +130,18 @@ find_segments <- function(est_slopes, var){
   slope_segments$feature <- dplyr::case_when(
     slope_segments$slope_sign == 1 ~ "increase",
     slope_segments$slope_sign == -1 ~ "decrease",
-    slope_segments$slope_sign == 0 & slope_segments$lag == 1 & slope_segments$lead == -1 ~ "peak",
-    slope_segments$slope_sign == 0 & slope_segments$lag == -1 & slope_segments$lead == 1 ~ "trough",
-    slope_segments$slope_sign == 0 & slope_segments$lag == 1 & slope_segments$lead == 1 ~ "increase_step",
-    slope_segments$slope_sign == 0 & slope_segments$lag == -1 & slope_segments$lead == -1 ~ "decrease_step",
-    slope_segments$slope_sign == 0 & is.na(slope_segments$lag) ~ "start_edge_flat",
-    slope_segments$slope_sign == 0 & is.na(slope_segments$lead) ~ "end_edge_flat"
+    slope_segments$slope_sign == 0 & slope_segments$lag == 1 & slope_segments$lead == -1 ~ "local_max",
+    slope_segments$slope_sign == 0 & slope_segments$lag == -1 & slope_segments$lead == 1 ~ "local_min",
+    slope_segments$slope_sign == 0 & slope_segments$lag == 1 & slope_segments$lead == 1 ~ "plateau",
+    slope_segments$slope_sign == 0 & slope_segments$lag == -1 & slope_segments$lead == -1 ~ "plateau",
+    slope_segments$slope_sign == 0 & is.na(slope_segments$lag & slope_segments$lead == 1) ~ "boundary_min",
+    slope_segments$slope_sign == 0 & is.na(slope_segments$lag & slope_segments$lead == -1) ~ "boundary_max",
+    slope_segments$slope_sign == 0 & is.na(slope_segments$lead & slope_segments$lag == 1) ~ "boundary_max",
+    slope_segments$slope_sign == 0 & is.na(slope_segments$lead & slope_segments$lag == -1) ~ "boundary_min"
   )
 
   #rejoining these defining features to the inital slopes dataframe
   est_slopes <- dplyr::full_join(est_slopes, slope_segments[, -2], by="seg_id")
-
-  # #defining peak and trough and edge-flat confidence intervals
-  # intervals <- dplyr::filter(slope_segments, feature == "peak" | feature == "trough" | grepl(pattern = "edge_flat", x = feature))
-  # intervals <- dplyr::group_by(intervals, feature)
-  # intervals <- dplyr:: summarize(intervals,
-  #                                conf_low = first())
 
   #renaming column names according to the variable name
   colnames(est_slopes)[which(colnames(est_slopes)=="x")] <- var
@@ -159,25 +155,3 @@ find_segments <- function(est_slopes, var){
 
   return(segments)
 }
-
-#
-#
-#
-#
-# quantify_sig_sin <- quantify_lextrema(gam_sig_sin)
-# #
-# # sig_sin_plot <- draw(gam_sig_sin, residuals = TRUE)
-#
-# smooth_sig_sin <- smooth_estimates(gam_sig_sin)
-# confint_sig_sin <- add_confint(smooth_sig_sin)
-#
-# ggplot()+
-#   geom_ribbon(aes(ymin = lower_ci, ymax = upper_ci, x = x),
-#                       alpha = 0.2, data=confint_sig_sin) +
-#   geom_line(aes(x=gam_sig_sin$model$x, y=gam_sig_sin$fitted.values-coef(gam_sig_sin)[1]))+
-#   geom_rect(aes(xmin=quantify_sig_sin$segment_summary$x_start, xmax=quantify_sig_sin$segment_summary$x_end, ymin=-Inf, ymax=Inf, fill=quantify_sig_sin$segment_summary$feature), alpha=.2)+
-#   geom_point(aes(x=quantify_sig_sin$x, y=quantify_sig_sin$slope_sign, colour=quantify_sig_sin$feature))
-#
-# ggplot(data=quantify_sig_sin$model_slopes)+
-#   geom_point(aes(x=x, y=estimate, colour=feature), size=0.1)+
-#   geom_ribbon(aes(x=x, ymin=conf.low, ymax=conf.high), alpha=0.5)
