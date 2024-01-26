aa0a99c6bb6662aace8d05f6020f961ac6cc43f1
NatalieDupont <natalie.dupont2000@gmail.com>
2024-01-26 17:24:38 -0500

    updating plotting code

--- R/visualize_lextrema.R
+++ R/visualize_lextrema.R
@@ -11,7 +11,7 @@
 #'
 #' @returns a ggplot object
 #' @export plot_lextrema
-plot_lextrema <- function(quant_segments, plot_deriv = TRUE, show_segs=c("peak", "trough")){
+plot_lextrema <- function(quant_segments, plot_deriv = TRUE, show_segs=c("local_max", "local_min"), show_segs_deriv = TRUE){
   shows_segs <- show_segs
   if(! "lextrema" %in% class(quant_segments)){
     stop("quant_segments must be an object of class lextrema produced by the quantify_lextrema function")
@@ -19,20 +19,53 @@ plot_lextrema <- function(quant_segments, plot_deriv = TRUE, show_segs=c("peak",
   if(! is.logical(plot_deriv)){
     stop("plot_deriv must be a logical TRUE/FALSE value")
   }
-  if (!all(show_segs %in% feature_codes) ){
+  if (!all(show_segs %in% feature_codes)){
     if( show_segs != "all"){
       stop(paste0("all show_segs values must be codes represented in the approved feature codes: ", paste0(feature_codes, collapse = ", ")))
     }
+  }
+  if(any(show_segs == "all")){
+    show_segs <- feature_codes
   }
 
   seg_data <- dplyr::filter(quant_segments$model_slopes, feature %in% show_segs)
 
   model_plot <- marginaleffects::plot_predictions(quant_segments$model, by=colnames(quant_segments$model$model)[2])
   model_plot <- model_plot+ggplot2::geom_line(data=seg_data,
-                                     ggplot2::aes(x = unlist(dplyr::select(seg_data, quant_segments$var)), y = predicted, group = seg_id, colour = feature))+
+                                     ggplot2::aes(x = unlist(dplyr::select(seg_data, quant_segments$var)), y = predicted, group = seg_id, colour = feature),
+                                     linewidth = 1.2)+
     ggplot2::theme_bw()
+
+  if(plot_deriv){
+
+    if(show_segs_deriv){
+      deriv_plot <- ggplot2::ggplot(data=quant_segments$model_slopes)+
+        ggplot2::geom_line(
+          ggplot2::aes(x=x, y=estimate))+
+        ggplot2::geom_ribbon(ggplot2::aes(x=x, ymin=conf.low, ymax=conf.high, group = seg_id, fill = feature), alpha=0.25)+
+        ggplot2::geom_hline(ggplot2::aes(yintercept=0), linetype=3, colour="grey40", linewidth=1.1)+
+        ggplot2::theme_bw()
+    } else{
+      deriv_plot <- ggplot2::ggplot(data=quant_segments$model_slopes)+
+        ggplot2::geom_line(
+          ggplot2::aes(x=x, y=estimate))+
+        ggplot2::geom_ribbon(ggplot2::aes(x=x, ymin=conf.low, ymax=conf.high), fill="black", alpha=0.25)+
+        ggplot2::geom_hline(ggplot2::aes(yintercept=0), linetype=3, colour="grey40", linewidth=1.1)+
+        ggplot2::theme_bw()
+    }
+  }
  ####DEPENDS ON Predictor variable always being in column 13, which is not always the case. Change to be variable dependent
-  return(model_plot)
+  print(model_plot)
+  if(plot_deriv){
+    print(deriv_plot)
+  }
+
+  if(!plot_deriv){
+    plots <- model_plot
+  }else {
+    plots <- list(model_plot = model_plot, deriv_plot = deriv_plot)
+  }
+  return(plots)
 }
 
 
